31309eb645b3472a37074ed0b00705dac7a56389
Alessandro1979-itac <muniz.caranha@gmail.com>
2020-04-18 15:38:00 -0400

    Criação de serviços e funções PHP

--- servicos/servicoCategoriaCompetidor.php
+++ servicos/servicoCategoriaCompetidor.php
@@ -1,1 +1,52 @@
 <?php
+
+function defineCategoriaCompetidor(string $nome, string $idade) : ?string
+{
+    $categotias = [];
+    $categotias[] = 'infantil';
+    $categotias[] = 'adolescente';
+    $categotias[] = 'adulto';
+
+    if ((validaNome($nome)) && (validaIdade($idade)))
+    {
+        removerMensagemErro();
+        if (($idade >= 6) && ($idade <= 12))
+        {
+            for ($i = 0; $i <= count($categotias); $i++)
+            {
+                if ($categotias[$i] == 'infantil')
+                {
+                    setarMensagemSucesso("O nadador ".$nome." compete na categoria ".$categotias[$i]);
+                    return null;
+                }
+            }
+        }
+        elseif (($idade >= 13) && ($idade <= 18))
+        {
+            for ($i = 0; $i <= count($categotias); $i++)
+            {
+                if ($categotias[$i] == 'adolescente')
+                {
+                    setarMensagemSucesso("O nadador: ".$nome." compete na categoria ".$categotias[$i]);
+                    return null;
+                }
+            }
+        }
+        else
+        {
+            for ($i = 0; $i <= count($categotias); $i++)
+            {
+                if ($categotias[$i] == 'adulto')
+                {
+                    setarMensagemSucesso("O nadador: ".$nome." compete na categoria ".$categotias[$i]);
+                    return null;
+                }
+            }
+        }
+    }
+    else
+    {
+        removerMensagemSucesso();
+        return obterMensagemErro();
+    }
+}
--- servicos/servicoValidacao.php
+++ servicos/servicoValidacao.php
@@ -1,1 +1,30 @@
 <?php
+
+function validaNome(string $nome) : bool
+{
+    if (empty($nome))
+    {
+        setarMensagemErro('O nome não pode ser vazio, por favor preencha-o!');
+        return false;
+    }
+    elseif (strlen($nome) < 3)
+    {
+        setarMensagemErro('O nome não pode conter menos de 3 caracteres!');
+        return false;
+    }
+    elseif (strlen($nome) > 40)
+    {
+        setarMensagemErro('O nome não pode conter mais 40 caracteres!');
+        return false;
+    }
+    return true;
+}
+
+function validaIdade(string $idade) : bool
+{
+    if ((!is_numeric($idade)) || ($idade <= 0)) {
+        setarMensagemErro('A idade não pode está vazia, e informe um número maior que zero!');
+        return false;
+    }
+        return true;
+}
